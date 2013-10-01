3a6ebb383820b421ceecf05977cd56bcd2c06026
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2013-10-01 19:29:13 +0300

    continue test when_expand_table

---
+++ src/include/tests/unit/common/hashtable/hashtable_helper.h
@@ -0,0 +1,23 @@
+
+#ifndef HASHTABLE_HELPER_H
+#define HASHTABLE_HELPER_H
+
+#define MAX_ALLOWED_DEVIATION (0.1)
+
+typedef struct SListItemCount
+{
+	uint       listId;	    
+	uint	   count;
+	float      deviation;
+} SListItemCount, *ListItemCount;
+
+typedef void (*assertListCount)(ListItemCount item);
+
+extern SListItemCount hashListItemsCount[1000];
+extern int            listsCount;
+
+void clearHashLists();
+void calculateHashListsLens(Hashtable tbl, assertListCount assertFunc);
+void checkListCountDeviation(ListItemCount item);
+
+#endif
