32cfa259c261229ab9fb42ca195d343a37cce4ad
Ruslan Iryk <rriryk@gmail.com>
2014-01-13 00:30:28 +0200

    continue memcontainermanager.c

--- memcontainermanager.c
+++ memcontainermanager.c
@@ -1,5 +1,171 @@
 #include "memcontainermanager.h"
 
+/* Calculates the number of a free list
+ * depending on size. The free list should 
+ * contain a memory chunk of length of a power of two.
+ */
+int calculateFreeListIndex(size_t   size)
+{
+	int			 ind;
+	int          minChunkSize = (1 << MIN_CHUNK_POWER_OF_2);
+    int          chunksCount;
+    int          large;
+
+	if (size <= minChunkSize)
+		return 0;
+
+	/* How many minChunks does the size contain? */
+	chunksCount = (size - 1) >> MIN_CHUNK_POWER_OF_2;
+
+	/* At this point we should calculate log2(chunksCount)
+	 * We should check if chunksCount is more than 256 or not.
+     * We divide it on 256 and see if the result is more than 0.
+	 */
+	large = chunksCount >> 8;
+    
+	/* chunksCount = large * (2^8) 
+	 * And when we take log2(chunksCount) = 
+	 * log2(large * (2^8)) = log2(large) + log2(2^8) = 
+     * log2(large) + 8;
+	 */
+	if (large > 0)
+        return Log2Table[large] + 8;
+
+    return Log2Table[large];
+}
+
+void* allocSetAlloc(
+    MemoryContainer          container, 
+	size_t                   size)
+{   
+    MemorySet	 set = (MemorySet)container;
+	MemoryBlock  block;
+	MemoryChunk	 chunk;
+
+    void*        chunkPtr;
+    int			 freeListInd;
+
+	size_t		 chunkSize;
+	size_t		 blockSize;
+
+	/* In this case the requested memory size 
+	 * is more than the max chunk size. So that
+	 * we can't allocate a memory chunk.
+	 * In this case we allocate a new whole block.
+	 */
+	if (size > memset->chunkMaxSize)
+	{
+        chunkSize = ALIGN_DEFAULT(size);
+		blockSize = chunkSize + MEM_BLOCK_SIZE + MEM_CHUNK_SIZE;
+		block     = (MemorySet)malloc(blockSize);
+
+		if (block == NULL)
+		   ; /* Should report the error. Now it is not implemented. */
+        
+		block->memset    = set;
+		block->freeStart = (char*)block + blockSize;
+		block->freeEnd   = block->freeStart;
+
+		chunk         = (MemoryChunk)((char*)block + MEM_BLOCK_SIZE);
+		chunk->memset = set;
+		chunk->size   = chunkSize;
+		chunk->sizeRequested = size;
+        
+		chunkPtr = MemoryChunkGetPointer(chunk);
+
+		/* If actual allocated size of memory is larger than
+		 * the requested size we mark the "unused space".
+		 */
+		if (size < chunkSize)
+            ((char*)chunkPtr)[size] = UNUSED_SPACE_MARK;
+
+		/* Insert the block into the head of 
+		 * the set's block list.
+		 */ 
+		if (set->blockList != NULL)
+		{
+			block->next = set->blockList->next;
+			set->blockList->next = block;
+		}
+		else
+		{
+			block->next = NULL;
+			set->blockList = block;
+		}
+
+		return chunkPtr;
+	}
+
+	/* The size of the chunk is too small to be an entire block.
+	 * In this case we should treat it as a chunk.
+	 * First of all we should look at the free list if 
+	 * there is a free chunk that can come up to our 
+	 * memory request.
+	 */
+    freeListInd = calculateFreeListIndex(size);
+	chunk       = set->freelist[freeListInd];
+
+	if (chunk != NULL)
+	{
+		/* We remove the chunk from the head of the free list. */
+		set->freelist[freeListInd] = (MemoryChunk)chunk->memsetorchunk;
+
+		/* MemSetOrChunk now points to the parent memory set */
+		chunk->memsetorchunk = (void*)set;
+        chunk->sizeRequested = size;		
+
+        chunkPtr = MemoryChunkGetPointer(chunk);
+
+        if (size < chunkSize)
+            ((char*)chunkPtr)[size] = UNUSED_SPACE_MARK;
+
+		return chunkPtr;
+	}
+
+	/* Now we should calculate the chunk size.
+	 * freeListInd = log2(chunksCount)
+	 * 2^(freeListInd) = 2^(log2(chunksCount))
+	 * chunksCount = 2 ^ freeListInd;
+	 * 1 << 10 = 2 ^ 10. So we should shift left 
+	 * on freeListInd positions
+	 * chunksCount = 1 << freeListInd;
+	 * And then we should multiple on 2 ^ MIN_CHUNK_POWER_OF_2
+	 */
+	chunkSize = (1 << MIN_CHUNK_POWER_OF_2) << freeListInd;
+
+	/* Take the first block in the blocks list */
+	block = set->blockList;
+
+	/* If block is not null we try to find out 
+	 * if there is a free space on it which
+	 * has size chunkSize.
+	 */
+	if (block != NULL)
+	{
+		size_t  availSpace = block->freeEnd - block->freeStart;
+
+		if (availSpace < (chunkSize + MEM_CHUNK_SIZE))
+		{
+			/* The availiable space does not come up 
+			 * for this chunk. But we can use this space in the future
+			 * Now we carve up this space into chunks and add it 
+			 * to the free list.
+			 */
+
+		}
+	}
+}
+
+
+void* allocateSpace(
+	MemoryContainer          container, 
+	size_t                   size)
+{
+	container->isReset = False;
+
+	return (*context->methods->alloc) (context, size);
+}
+
 MemoryContainer allocateNewMemCont(
 	    MemoryContainer     parent,
 		char*               name,
--- memory.h
+++ memory.h
@@ -1,10 +1,24 @@
 
 
+/* The smallest chunk of memory is 8 bytes.
+ * 8 = 2^3. So that the smallest power of 2 is 3.
+ */
+#define MIN_CHUNK_POWER_OF_2	    3	
+#define MEMORY_SET_FREELISTS_NUM	11
+
 typedef enum MemContType
 {
    MCT_Invalid         = 0,
    MCT_MemoryContainer = 10
 } MemContType;
+
+#define MEM_BLOCK_SIZE	   ALIGN_DEFAULT(sizeof(SMemoryBlock))
+#define MEM_CHUNK_SIZE	   ALIGN_DEFAULT(sizeof(SMemoryChunk))
+
+#define UNUSED_SPACE_MARK  0x7E  /* In dec it is 126, in binary - 0111 1110 */
+
+#define MemoryChunkGetPointer(chunk)	\
+      ((void*)(((char*)(chunk)) + MEM_CHUNK_SIZE))
 
 /* Memory allocations are united into memory container.
  * Each container includes chunks of memory. And when we delete 
@@ -42,6 +56,97 @@ typedef struct SMemoryContainer
 	char*                       name;			/* container name  */
 	Bool		                isReset;		
 } SMemoryContainer, *MemoryContainer;
+
+
+typedef struct SMemorySet
+{
+	/* SetContainer inherits from MemoryContainer 
+	 * So that the first structure field should be 
+	 * a reference to the base class.
+	 */
+	SMemoryContainer   baseMemCont;	
+
+	/* head of list of blocks in the memory set */
+	MemoryBlock	       blockList;	
+
+	/* Lists of free memory chunks 
+	 * Each chunk free list contains pieces of memory
+	 * of a defunite size and this size is a power of 2
+	 */
+	MemoryChunk	       freelist[MEMORY_SET_FREELISTS_NUM];	
+
+	/* initial block size */
+	size_t		       initBlockSize;	
+
+    /* maximum block size */
+	size_t		       maxBlockSize;	
+
+    /* next block size to allocate */
+	size_t		       nextBlockSize;	
+
+    /* effective chunk size limit */
+	size_t		       chunkMaxSize;
+	MemoryBlock	       keeper;	
+} SMemorySet, *MemorySet;
+
+
+typedef struct SMemoryBlock
+{
+	MemorySet	  memset;		  /* MemorySet which the block belongs to */
+	MemoryBlock	  next;			  /* next block in the memory set's blocks list */
+	char*         freeStart;	  /* start of free space in this block */
+	char*         freeEnd;		  /* end of free space in this block */
+} SMemoryBlock, *MemoryBlock;
+
+
+typedef struct SMemoryChunk
+{
+	/* If the chunk is inside a free list it points to 
+	 * the next chunk in the free list. If this chunk 
+	 * is withdrawn from the free list it points to 
+	 * the parent set.
+	 */
+	void*         memsetorchunk;
+
+	/* The size of memory which is actually used 
+	 * in the chunk.
+	 */
+	size_t		  size;
+
+	/* The size of memory which has been actually 
+	 * requested by a user.
+	 */
+	size_t		  sizeRequested;
+} SMemoryChunk, *MemoryChunk;
+
+
+
+/* If we are requesting for a memory of size x
+ * where 0 <= x <= 256.  256 = 2^8.
+ * We would like only to allocate memory chunks 
+ * that are of power of 2 size. We have 9 possible values:
+ * 2^0, 2^1, 2^2, 2^3, 2^4, 2^5, 2^6, 2^7, 2^8
+ * 1,   2,   4,   8,   16,  32,  64,  128, 256
+ * 
+ * If we request some size of memory which is not a power of 2,
+ * we should round it up to the nearest larger power of 2.
+ * For example if we request 45. we should allocate 64.
+ */
+#define SeqOf16CopiesOf(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
+
+/* This table matches n to log2(n) + 1 
+ * It has been created to avoid calculating it
+ * all time.
+ */
+unsigned char Log2Table[256] =
+{
+	0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
+	SeqOf16CopiesOf(5), SeqOf16CopiesOf(6), SeqOf16CopiesOf(6), SeqOf16CopiesOf(7), 
+	SeqOf16CopiesOf(7), SeqOf16CopiesOf(7), SeqOf16CopiesOf(7),
+	SeqOf16CopiesOf(8), SeqOf16CopiesOf(8), SeqOf16CopiesOf(8), SeqOf16CopiesOf(8), 
+	SeqOf16CopiesOf(8), SeqOf16CopiesOf(8), SeqOf16CopiesOf(8), SeqOf16CopiesOf(8)
+};
+
 
 
 
