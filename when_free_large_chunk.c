1c2b67c166d0a799ab42b86cd2137accb345ccd1
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2014-02-10 18:24:57 +0200

    finished Rickter and started memory management description

--- when_free_large_chunk.c
+++ when_free_large_chunk.c
@@ -38,6 +38,8 @@ GIVEN(free_large_chunk)
 		 1024);
 
 	mem_flc = mm_flc->allocateMemory(mm_flc, ms_flc, 150);
+    mem_flc = mm_flc->allocateMemory(mm_flc, ms_flc, 150);
+    mem_flc = mm_flc->allocateMemory(mm_flc, ms_flc, 150);
 }
 
 WHEN(free_large_chunk)
@@ -54,12 +56,39 @@ TEST_TEAR_DOWN(free_large_chunk)
 
 TEST(free_large_chunk, then_the_chunk_block_must_be_freed)
 {
-	
+	int          blockSize      = MEM_BLOCK_SIZE;
+	int          chunkSize      = MEM_CHUNK_SIZE;
+	MemoryBlock  chunkBlock     = (MemoryBlock)((char*)mem_flc - blockSize - chunkSize);
+
+    TEST_ASSERT_EQUAL_UINT32(free_count, 1);
+    TEST_ASSERT_EQUAL_UINT32(free_mem_addrs[0], (int)chunkBlock);
 }
 
 TEST(free_large_chunk, then_the_chunk_block_must_be_deleted_from_list)
 {
-	
+	int          blockCount     = 0;
+	MemoryBlock  block          = ms_flc->blockList;
+    MemoryBlock  chunkBlock     = (char*)mem_flc - MEM_BLOCK_SIZE - MEM_CHUNK_SIZE;
+    Bool         isBlockPresent = False;
+
+	while (block != NULL)
+	{
+		blockCount++;
+		block = block->next;
+	}
+
+    TEST_ASSERT_EQUAL_UINT32(blockCount, 2);
+
+	block = ms_flc->blockList;
+	while (block != NULL)
+	{
+		if (block == chunkBlock)
+            isBlockPresent = True;
+
+		block = block->next;
+	}
+
+	TEST_ASSERT_FALSE(isBlockPresent);
 }
 
 TEST_GROUP_RUNNER(free_large_chunk)
